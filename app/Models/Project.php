b88ea82ee4702805f400e1146b2858f132c2f1ef
matteopoliti <matteo.politi17@gmail.com>
2024-04-03 11:13:16 +0200

    First commit

---
+++ app/Models/Project.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'project_start_date',
+        'slug',
+        'cover_image',
+        'type_id'
+    ];
+
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+}
